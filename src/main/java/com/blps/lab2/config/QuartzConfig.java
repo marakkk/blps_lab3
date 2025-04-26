254d146bd650070ba008fac0d13a701dee424ee2
marakkk <marakobzar2304@gmail.com>
2025-04-27 01:39:26 +0300

    fix logic for scheduler

--- src/main/java/com/blps/lab2/config/QuartzConfig.java
+++ src/main/java/com/blps/lab2/config/QuartzConfig.java
@@ -2,17 +2,32 @@ package com.blps.lab2.config;
 
 import com.blps.lab2.scheduler.PayoutJob;
 import lombok.RequiredArgsConstructor;
+import org.postgresql.ds.PGSimpleDataSource;
 import org.quartz.*;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSourceScriptDatabaseInitializer;
+import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
+import org.springframework.boot.autoconfigure.quartz.QuartzTransactionManager;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
 @Configuration
 @RequiredArgsConstructor
 public class QuartzConfig {
+    @Value("${spring.datasource.payments.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.payments.user}")
+    private String dbUser;
+
+    @Value("${spring.datasource.payments.password}")
+    private String dbPassword;
 
     private final PayoutJobProperties payoutJobProperties;
 
@@ -34,11 +49,30 @@ public class QuartzConfig {
     }
 
     @Bean
-    public SchedulerFactoryBean schedulerFactoryBean(@Qualifier("paymentsDataSource") DataSource dataSource) {
-        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+    @QuartzTransactionManager
+    public PlatformTransactionManager quartzTransactionManager(
+            @QuartzDataSource DataSource dataSource
+    ) {
+        return new JdbcTransactionManager(dataSource);
+    }
 
-        factory.setDataSource(dataSource);
+    @Bean
+    @QuartzDataSource
+    public DataSource quartzDataSource() {
+        return DataSourceBuilder
+                .create()
+                .username(dbUser)
+                .password(dbPassword)
+                .url(dbUrl)
+                .driverClassName("org.postgresql.Driver")
+                .type(PGSimpleDataSource.class)
+                .build();
+    }
 
-        return factory;
+    @Bean
+    public QuartzDataSourceScriptDatabaseInitializer quartzDataSourceScriptDatabaseInitializer(
+            @QuartzDataSource DataSource quartzDataSource,
+            QuartzProperties properties) {
+        return new QuartzDataSourceScriptDatabaseInitializer(quartzDataSource, properties);
     }
 }
